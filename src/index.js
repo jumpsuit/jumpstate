79d1c7841a7af2b7e1c8c88c54eb166125375bd8
Tanner Linsley <tannerlinsley@gmail.com>
2016-11-07 12:25:37 -0700

    Import and use Hook from jumpsuit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import State, { StateDefaults } from './state'
 import Effect from './effect'
+import Hook from './hook'
 import Actions from './actions'
 import CreateJumpstateMiddleware, { getState, dispatch } from './middleware'
 
 export {
   State,
   Effect,
+  Hook,
   Actions,
   StateDefaults,
   CreateJumpstateMiddleware,
